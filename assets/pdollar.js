0d2e0c09dfc06698ccbcecf8b73d9787aa314a7e
Md Anisur Rahman <anistuhin@gmail.com>
2024-11-14 23:38:26 -0600

    tried to debug js pdollar

--- assets/pdollar.js
+++ assets/pdollar.js
@@ -57,52 +57,52 @@
  * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
-**/
+ **/
 //
 // Point class
 //
 function Point(x, y, id) // constructor
 {
-	this.X = x;
-	this.Y = y;
-	this.ID = id; // stroke ID to which this point belongs (1,2,3,etc.)
+    this.X = x;
+    this.Y = y;
+    this.ID = id; // stroke ID to which this point belongs (1,2,3,etc.)
 }
 //
 // PointCloud class: a point-cloud template
 //
 function PointCloud(name, points) // constructor
 {
-	this.Name = name;
-	this.Points = Resample(points, NumPoints);
-	this.Points = Scale(this.Points);
-	this.Points = TranslateTo(this.Points, Origin);
+    this.Name = name;
+    this.Points = Resample(points, NumPoints);
+    this.Points = Scale(this.Points);
+    this.Points = TranslateTo(this.Points, Origin);
 }
 //
 // Result class
 //
 function Result(name, score, ms) // constructor
 {
-	this.Name = name;
-	this.Score = score;
-	this.Time = ms;
+    this.Name = name;
+    this.Score = score;
+    this.Time = ms;
 }
 //
 // PDollarRecognizer constants
 //
 const NumPoints = 32;
-const Origin = new Point(0,0,0);
+const Origin = new Point(0, 0, 0);
 //
 // PDollarRecognizer class
 //
 function PDollarRecognizer() // constructor
 {
-	//
-	// one predefined point-cloud for each gesture
-	//
-	this.PointClouds = new Array();
+    //
+    // one predefined point-cloud for each gesture
+    //
+    this.PointClouds = new Array();
 
-	this.ProcessGesturesFile = function(file) {
-	    const lines = file.split('\n');
+    this.ProcessGesturesFile = function(file) {
+        const lines = file.split('\n');
 
         let points = new Array();
         let name = "";
@@ -110,188 +110,187 @@ function PDollarRecognizer() // constructor
         let first = true;
         let curr = 0;
 
-	    lines.forEach((line) => {
-	        if (line.includes(',')) {
-	            var temp = line.split(',');
-	            var point = new Point(parseFloat(temp[0]), parseFloat(temp[1]), stroke_num);
-	            points.push(point);
-	        }
-	        else if (line.trim() === 'BEGIN') {
-	            stroke_num = 0;
-	        } else if (line.trim() === 'END') {
-	            stroke_num += 1;
-	        } else {
-	            if (!first) {
-	                var temp = new PointCloud(name, points);
-	                this.PointClouds.push(temp);
-	                curr += 1;
-	            }
-	            name = line.trim();
-	            first = false;
-	        }
-	    });
+        lines.forEach((line) => {
+            if (line.includes(',')) {
+                let temp = line.split(',');
+                let point = new Point(parseFloat(temp[0]), parseFloat(temp[1]), stroke_num);
+                points.push(point);
+            } else if (line.trim() === 'BEGIN') {
+                stroke_num = 0;
+            } else if (line.trim() === 'END') {
+                stroke_num += 1;
+            } else {
+                if (!first) {
+                    let temp = new PointCloud(name, points);
+                    this.PointClouds.push(temp);
+                    curr += 1;
+                }
+                name = line.trim();
+                first = false;
+            }
+        });
 
-	    var temp = new PointCloud(name, points);
+        let temp = new PointCloud(name, points);
         this.PointClouds.push(temp);
-        console.log("Done loading gesture file");
-	}
-	this.Recognize = function(points)
-	{
-	    let pointsAsJsonString = JSON.stringify(points);
-	    var pointsArray = JSON.parse(pointsAsJsonString);
+        console.log(this.PointClouds);
+    }
+    this.Recognize = function(points) {
+        let pointsAsJsonString = JSON.stringify(points);
+        var pointsArray = JSON.parse(pointsAsJsonString);
         let pointCloud = pointsArray.map(p => new Point(p.x, p.y, p.ID));
-		var candidate = new PointCloud("", pointCloud);
+        var candidate = new PointCloud("", pointCloud);
 
-		var u = -1;
-		var score = +Infinity;
-		for (var i = 0; i < this.PointClouds.length; i++) // for each point-cloud template
-		{
-		    var d = GreedyCloudMatch(candidate.Points, this.PointClouds[i]);
-			if (d < score) {
-			    score = d; // best (least) distance
-				u = i; // point-cloud index
-			}
-		}
+        var u = -1;
+        var score = +Infinity;
+        for (var i = 0; i < this.PointClouds.length; i++) // for each point-cloud template
+        {
+            var d = GreedyCloudMatch(candidate.Points, this.PointClouds[i]);
+            if (d < score) {
+                score = d; // best (least) distance
+                u = i; // point-cloud index
+            }
+        }
         score = Math.max((2.0 - score) / 2.0, 0.0);
-		return (u == -1 || score == 0.0) ? "No match" : this.PointClouds[u].Name;
-	}
-	this.AddGesture = function(name, points)
-	{
-		this.PointClouds[this.PointClouds.length] = new PointCloud(name, points);
-		var num = 0;
-		for (var i = 0; i < this.PointClouds.length; i++) {
-			if (this.PointClouds[i].Name == name)
-				num++;
-		}
-		return num;
-	}
-	this.DeleteUserGestures = function()
-	{
-		this.PointClouds.length = 0; // clears any beyond the original set
-		return NumPointClouds;
-	}
+        return (u == -1 || score == 0.0) ? "No match" : this.PointClouds[u].Name;
+    }
+    this.AddGesture = function(name, points) {
+        this.PointClouds[this.PointClouds.length] = new PointCloud(name, points);
+        var num = 0;
+        for (var i = 0; i < this.PointClouds.length; i++) {
+            if (this.PointClouds[i].Name == name)
+                num++;
+        }
+        return num;
+    }
+    this.DeleteUserGestures = function() {
+        this.PointClouds.length = 0; // clears any beyond the original set
+        return NumPointClouds;
+    }
 }
 //
 // Private helper functions from here on down
 //
-function GreedyCloudMatch(points, P)
-{
-	var e = 0.50;
-	var step = Math.floor(Math.pow(points.length, 1.0 - e));
-	var min = +Infinity;
-	for (var i = 0; i < points.length; i += step) {
-		var d1 = CloudDistance(points, P.Points, i);
-		var d2 = CloudDistance(P.Points, points, i);
-		min = Math.min(min, Math.min(d1, d2)); // min3
-	}
-	return min;
+function GreedyCloudMatch(points, P) {
+    var e = 0.50;
+    var step = Math.floor(Math.pow(points.length, 1.0 - e));
+    var min = +Infinity;
+    for (var i = 0; i < points.length; i += step) {
+        var d1 = CloudDistance(points, P.Points, i);
+        var d2 = CloudDistance(P.Points, points, i);
+        min = Math.min(min, Math.min(d1, d2)); // min3
+    }
+    return min;
 }
-function CloudDistance(pts1, pts2, start)
-{
-	var matched = new Array(pts1.length); // pts1.length == pts2.length
-	for (var k = 0; k < pts1.length; k++)
-		matched[k] = false;
-	var sum = 0;
-	var i = start;
-	do
-	{
-		var index = -1;
-		var min = +Infinity;
-		for (var j = 0; j < matched.length; j++)
-		{
-			if (!matched[j]) {
-				var d = Distance(pts1[i], pts2[j]);
-				if (d < min) {
-					min = d;
-					index = j;
-				}
-			}
-		}
-		matched[index] = true;
-		var weight = 1 - ((i - start + pts1.length) % pts1.length) / pts1.length;
-		sum += weight * min;
-		i = (i + 1) % pts1.length;
-	} while (i != start);
-	return sum;
+
+function CloudDistance(pts1, pts2, start) {
+    var matched = new Array(pts1.length); // pts1.length == pts2.length
+    for (var k = 0; k < pts1.length; k++)
+        matched[k] = false;
+    var sum = 0;
+    var i = start;
+    do {
+        var index = -1;
+        var min = +Infinity;
+        for (var j = 0; j < matched.length; j++) {
+            if (!matched[j]) {
+                var d = Distance(pts1[i], pts2[j]);
+                if (d < min) {
+                    min = d;
+                    index = j;
+                }
+            }
+        }
+        matched[index] = true;
+        var weight = 1 - ((i - start + pts1.length) % pts1.length) / pts1.length;
+        sum += weight * min;
+        i = (i + 1) % pts1.length;
+    } while (i != start);
+    return sum;
 }
-function Resample(points, n)
-{
-	var I = PathLength(points) / (n - 1); // interval length
-	var D = 0.0;
-	var newpoints = new Array(points[0]);
-	for (var i = 1; i < points.length; i++)
-	{
-		if (points[i].ID == points[i-1].ID)
-		{
-			var d = Distance(points[i-1], points[i]);
-			if ((D + d) >= I)
-			{
-				var qx = points[i-1].X + ((I - D) / d) * (points[i].X - points[i-1].X);
-				var qy = points[i-1].Y + ((I - D) / d) * (points[i].Y - points[i-1].Y);
-				var q = new Point(qx, qy, points[i].ID);
-				newpoints[newpoints.length] = q; // append new point 'q'
-				points.splice(i, 0, q); // insert 'q' at position i in points s.t. 'q' will be the next i
-				D = 0.0;
-			}
-			else D += d;
-		}
-	}
-	if (newpoints.length == n - 1) // sometimes we fall a rounding-error short of adding the last point, so add it if so
-		newpoints[newpoints.length] = new Point(points[points.length - 1].X, points[points.length - 1].Y, points[points.length - 1].ID);
-	return newpoints;
+
+function Resample(points, n) {
+    var I = PathLength(points) / (n - 1); // interval length
+    var D = 0.0;
+    var newpoints = new Array(points[0]);
+    for (var i = 1; i < points.length; i++) {
+        if (points[i].ID == points[i - 1].ID) {
+            var d = Distance(points[i - 1], points[i]);
+            if ((D + d) >= I) {
+                var qx = points[i - 1].X + ((I - D) / d) * (points[i].X - points[i - 1].X);
+                var qy = points[i - 1].Y + ((I - D) / d) * (points[i].Y - points[i - 1].Y);
+                var q = new Point(qx, qy, points[i].ID);
+                newpoints[newpoints.length] = q; // append new point 'q'
+                points.splice(i, 0, q); // insert 'q' at position i in points s.t. 'q' will be the next i
+                D = 0.0;
+            } else D += d;
+        }
+    }
+    if (newpoints.length == n - 1) // sometimes we fall a rounding-error short of adding the last point, so add it if so
+        newpoints[newpoints.length] = new Point(points[points.length - 1].X, points[points.length - 1].Y, points[points.length - 1].ID);
+    return newpoints;
 }
-function Scale(points)
-{
-	var minX = +Infinity, maxX = -Infinity, minY = +Infinity, maxY = -Infinity;
-	for (var i = 0; i < points.length; i++) {
-		minX = Math.min(minX, points[i].X);
-		minY = Math.min(minY, points[i].Y);
-		maxX = Math.max(maxX, points[i].X);
-		maxY = Math.max(maxY, points[i].Y);
-	}
-	var size = Math.max(maxX - minX, maxY - minY);
-	var newpoints = new Array();
-	for (var i = 0; i < points.length; i++) {
-		var qx = (points[i].X - minX) / size;
-		var qy = (points[i].Y - minY) / size;
-		newpoints[newpoints.length] = new Point(qx, qy, points[i].ID);
-	}
-	return newpoints;
+
+function Scale(points) {
+    var minX = +Infinity,
+        maxX = -Infinity,
+        minY = +Infinity,
+        maxY = -Infinity;
+    for (var i = 0; i < points.length; i++) {
+        minX = Math.min(minX, points[i].X);
+        minY = Math.min(minY, points[i].Y);
+        maxX = Math.max(maxX, points[i].X);
+        maxY = Math.max(maxY, points[i].Y);
+    }
+    var size = Math.max(maxX - minX, maxY - minY);
+    var newpoints = new Array();
+    for (var i = 0; i < points.length; i++) {
+        var qx = (points[i].X - minX) / size;
+        var qy = (points[i].Y - minY) / size;
+        newpoints[newpoints.length] = new Point(qx, qy, points[i].ID);
+    }
+    return newpoints;
 }
+
 function TranslateTo(points, pt) // translates points' centroid to pt
 {
-	var c = Centroid(points);
-	var newpoints = new Array();
-	for (var i = 0; i < points.length; i++) {
-		var qx = points[i].X + pt.X - c.X;
-		var qy = points[i].Y + pt.Y - c.Y;
-		newpoints[newpoints.length] = new Point(qx, qy, points[i].ID);
-	}
-	return newpoints;
+    var c = Centroid(points);
+    var newpoints = new Array();
+    for (var i = 0; i < points.length; i++) {
+        var qx = points[i].X + pt.X - c.X;
+        var qy = points[i].Y + pt.Y - c.Y;
+        newpoints[newpoints.length] = new Point(qx, qy, points[i].ID);
+    }
+    return newpoints;
 }
-function Centroid(points)
-{
-	var x = 0.0, y = 0.0;
-	for (var i = 0; i < points.length; i++) {
-		x += points[i].X;
-		y += points[i].Y;
-	}
-	x /= points.length;
-	y /= points.length;
-	return new Point(x, y, 0);
+
+function Centroid(points) {
+    var x = 0.0,
+        y = 0.0;
+    for (var i = 0; i < points.length; i++) {
+        x += points[i].X;
+        y += points[i].Y;
+    }
+    x /= points.length;
+    y /= points.length;
+    return new Point(x, y, 0);
 }
+
 function PathLength(points) // length traversed by a point path
 {
-	var d = 0.0;
-	for (var i = 1; i < points.length; i++) {
-		if (points[i].ID == points[i-1].ID)
-			d += Distance(points[i-1], points[i]);
-	}
-	return d;
+    var d = 0.0;
+    for (var i = 1; i < points.length; i++) {
+        if (points[i].ID == points[i - 1].ID)
+            d += Distance(points[i - 1], points[i]);
+    }
+    return d;
 }
+
 function Distance(p1, p2) // Euclidean distance between two points
 {
-	var dx = p2.X - p1.X;
-	var dy = p2.Y - p1.Y;
-	return Math.sqrt(dx * dx + dy * dy);
+    var dx = p2.X - p1.X;
+    var dy = p2.Y - p1.Y;
+    return Math.sqrt(dx * dx + dy * dy);
 }
+
+let pdollar = new PDollarRecognizer();
+pdollar.ProcessGesturesFile(file);
